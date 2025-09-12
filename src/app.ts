738de80fc3d1aaeb4b73e9628b25b63e226f08be
Priyanku Gogoi <workwithpriyanku@gmail.com>
2025-09-12 20:04:21 +0530

    job scheduled

--- src/app.ts
+++ src/app.ts
@@ -25,3 +25,15 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`server running on port ${PORT}`);
 })
+
+// hourly fetching of the latest stock prices
+// shouldn't use this for production. 
+// high risk of duplicate execution if multiple instances are ran
+// better to use a cron job or something similar
+import * as schedule from 'node-schedule';
+import { StockPriceService } from './services/stockPriceService';
+
+schedule.scheduleJob('0 * * * *', async () => {
+    console.log('updating stock prices...')
+    await StockPriceService.updateAllStockPrices();
+})
